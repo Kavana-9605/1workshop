5c936d920128876c11effd6cc2674dddf5685a8a
Kavana-9605 <smkavana83@gmail.com>
2026-03-14 12:08:43 +0530

    Modified 2 files add.py & mul.py

--- add.py
+++ add.py
@@ -1,4 +1,8 @@
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
-def add(int a, int b):
+def add(a: int, b:int):
     c = a + b
+    return c
+
+result = add(a,b)
+print("Sum: ", result)
--- mul.py
+++ mul.py
@@ -1,4 +1,4 @@
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
-def mul(int a, int b):
+def mul(a:int, b: int):
     c = a + b
